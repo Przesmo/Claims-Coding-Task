ba9cb054b99f2af98a36bab9e4c8ff043d94b48f
Przesmo <przesmolana@gmail.com>
2024-06-11 18:05:17 +0200

    Added handling ClaimNotCovered exception in Controller

--- src/Insurance/Insurance.Host/Controllers/ClaimsController.cs
+++ src/Insurance/Insurance.Host/Controllers/ClaimsController.cs
@@ -1,4 +1,7 @@
 using Auditing.Infrastructure;
+using Insurance.Application.Exceptions;
+using Insurance.Application.Services;
+using Insurance.Host.Messages.Commands;
 using Insurance.Host.Messages.Queries;
 using Insurance.Infrastructure.Repositories.Claims;
 using Microsoft.AspNetCore.Mvc;
@@ -9,34 +12,47 @@ namespace Insurance.Host.Controllers;
 [Route("[controller]")]
 public class ClaimsController : ControllerBase
 {
+    private readonly IClaimsService _claimsService;
     private readonly IClaimsRepository _claimsRepository;
     private readonly Auditer _auditer;
     private readonly ILogger<ClaimsController> _logger;
 
     public ClaimsController(
+        IClaimsService claimsService,
         IClaimsRepository claimsContext,
         AuditContext auditContext,
-        ILogger<ClaimsController> logger)
+        ILogger<ClaimsController> logger
+        )
     {
+        _claimsService = claimsService;
         _claimsRepository = claimsContext;
         _auditer = new Auditer(auditContext);
         _logger = logger;
     }
 
     [HttpGet]
-    public async Task<IEnumerable<Claim>> GetAllAsync([FromQuery]GetClaims query)
+    public async Task<IEnumerable<Claim>> GetAllAsync([FromQuery] GetClaims query)
     {
         return await _claimsRepository.GetAllAsync(query.Offset, query.Limit);
     }
 
     [HttpPost]
     //To Do: Standarize when Action result when Task are returned etc.
-    public async Task<ActionResult> CreateAsync(Claim claim)
+    public async Task<ActionResult> CreateAsync([FromBody] CreateClaim command)
     {
-        claim.Id = Guid.NewGuid().ToString();
-        await _claimsRepository.CreateAsync(claim);
-        _auditer.AuditClaim(claim.Id, "POST");
-        return Ok(claim);
+        try
+        {
+            var claim = await _claimsService.CreateAsync(command);
+            //ToDo: Move Audit to Application layer
+            _auditer.AuditClaim(claim.Id, "POST");
+            return Ok(claim);
+        }
+        //ToDo: Move it error handler
+        catch (ClaimNotCoveredException ex)
+        {
+            _logger.LogError(ex, $"Claim cannot be created for cover: {command.CoverId}");
+            return BadRequest("Claim created date is not covered");
+        }
     }
 
     [HttpDelete("{id}")]
