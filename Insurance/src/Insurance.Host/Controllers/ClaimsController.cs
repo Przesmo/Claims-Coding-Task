f832d2f4d2a68d076321eebe3a79efafc302ccb3
Przesmo <przesmolana@gmail.com>
2024-06-20 15:06:04 +0200

    cleaned up ClaimsController

--- Insurance/src/Insurance.Host/Controllers/ClaimsController.cs
+++ Insurance/src/Insurance.Host/Controllers/ClaimsController.cs
@@ -1,6 +1,5 @@
 using Asp.Versioning;
 using Insurance.Application.DTOs;
-using Insurance.Application.Exceptions;
 using Insurance.Application.Messages.Commands;
 using Insurance.Application.Messages.Queries;
 using Insurance.Application.Services;
@@ -15,15 +14,9 @@ public class ClaimsController : ControllerBase
 {
     private readonly IClaimsService _claimsService;
 
-    private readonly ILogger<ClaimsController> _logger;
-
-    public ClaimsController(
-        IClaimsService claimsService,
-        ILogger<ClaimsController> logger
-        )
+    public ClaimsController(IClaimsService claimsService)
     {
         _claimsService = claimsService;
-        _logger = logger;
     }
 
     [HttpGet]
@@ -31,31 +24,14 @@ public class ClaimsController : ControllerBase
         await _claimsService.GetAllAsync(query);
 
     [HttpPost]
-    //To Do: Standarize when Action result when Task are returned etc.
-    public async Task<ClaimDTO> CreateAsync([FromBody] CreateClaim command)
-    {
-        return await _claimsService.CreateAsync(command);
-        //It will be moved to handler
-        /*try
-        {
-        }
-        //ToDo: Move it error handler
-        catch (ClaimNotCoveredException ex)
-        {
-            _logger.LogError(ex, $"Claim cannot be created for cover: {command.CoverId}");
-           // return BadRequest("Claim created date is not covered");
-        }*/
-    }
+    public async Task<ClaimDTO> CreateAsync([FromBody] CreateClaim command) =>
+        await _claimsService.CreateAsync(command);
 
     [HttpDelete("{id}")]
-    public async Task DeleteAsync([FromRoute] DeleteClaim command)
-    {
+    public async Task DeleteAsync([FromRoute] DeleteClaim command) =>
         await _claimsService.DeleteAsync(command);
-    }
 
     [HttpGet("{id}")]
-    public async Task<ClaimDTO?> GetAsync([FromRoute] GetClaim query)
-    {
-        return await _claimsService.GetAsync(query);
-    }
+    public async Task<ClaimDTO?> GetAsync([FromRoute] GetClaim query) =>
+        await _claimsService.GetAsync(query);
 }
