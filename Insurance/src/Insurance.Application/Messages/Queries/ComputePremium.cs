e3d1ad159f87585947ca3f9c270003d2cdaae6c8
Przesmo <przesmolana@gmail.com>
2024-06-22 13:29:24 +0200

    added extra validation for compute premium query

--- Insurance/src/Insurance.Application/Messages/Queries/ComputePremium.cs
+++ Insurance/src/Insurance.Application/Messages/Queries/ComputePremium.cs
@@ -6,9 +6,13 @@ namespace Insurance.Application.Messages.Queries;
 public class ComputePremium
 {
     [Required]
+    [DateIsGreaterThanToday]
     public DateTime StartDate { get; set; }
+
     [Required]
+    [DateGreaterThan("StartDate")]
     public DateTime EndDate { get; set; }
+
     [Required]
     public CoverType CoverType { get; set; }
 }
